cb0340625336eef61342edf62ea0caeea3ecb4ab
judenilson <judenilson@hotmail.com>
2023-10-30 21:13:25 -0300

    Configuring Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gym_app/screens/authenticate_screen.dart';
 // import 'package:gym_app/screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
